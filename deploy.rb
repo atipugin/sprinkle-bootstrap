8810840abe9e7f8b62b2f58b02b9ce3dd31c9ed6
Alexander Tipugin <atipugin@gmail.com>
2012-12-29 15:26:19 +0400

    Use ssh keys for authorization

--- deploy.rb
+++ deploy.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), 'base')
 
 default_run_options[:pty] = true
+ssh_options[:keys] = [File.join(ENV['HOME'], '.ssh', 'id_rsa')]
 
 set :user, $config[:user]
 set :password, $config[:password]
